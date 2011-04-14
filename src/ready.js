d00888028be66d2aad9d36c49a75a08e67d311c0
Dustin Diaz <polvero@gmail.com>
2011-04-14 13:27:11 -0700

    boosh

---
+++ src/ready.js
@@ -0,0 +1,53 @@
+!function (document) {
+  var loaded = 0, fns = [], ol,
+      testEl = doc.createElement('a'),
+      domContentLoaded = 'DOMContentLoaded', readyState = 'readyState',
+      onreadystatechange = 'onreadystatechange';
+
+  if (!doc[readyState] && doc.addEventListener) {
+    doc.addEventListener(domContentLoaded, function fn() {
+      doc.removeEventListener(domContentLoaded, fn, false);
+      doc[readyState] = "complete";
+    }, f);
+    doc[readyState] = "loading";
+  }
+  function again(fn) {
+    setTimeout(function() {
+      domReady(fn);
+    }, 50);
+  }
+
+  testEl.doScroll && doc.attachEvent(onreadystatechange, (ol = function ol() {
+    /^c/.test(doc[readyState]) &&
+    (loaded = 1) &&
+    !doc.detachEvent(onreadystatechange, ol) && !function () {
+      for (var i = 0, l = fns.length; i < l; i++) {
+        fns[i]();
+      }
+    }();
+  }));
+
+  var domReady = testEl.doScroll ?
+    function (fn) {
+      self != top ?
+        !loaded ?
+          fns.push(fn) :
+          fn() :
+        !function () {
+          try {
+            testEl.doScroll('left');
+          } catch (e) {
+            return again(fn);
+          }
+          fn();
+        }();
+    } :
+    function (fn) {
+      re.test(doc[readyState]) ? fn() : again(fn);
+    };
+
+    (typeof module !== 'undefined') && module.exports ?
+      (module.exports = {domReady: domReady}) :
+      (window.domReady = domReady);
+
+}(document);
