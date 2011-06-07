be81b957cde1584dd3f469bc33e396b19f3d44c8
Dustin Diaz <polvero@gmail.com>
2011-06-07 12:56:13 -0700

    change bridge for ready only

--- src/ender.js
+++ src/ender.js
@@ -5,13 +5,5 @@
       domReady(f);
       return this;
     }
-  }, true)
-  var _$ = $;
-  $ = function (s, r) {
-    if (typeof s == 'function') {
-      domReady(s);
-    } else {
-      return _$(s, r);
-    }
-  };
+  }, true);
 }(ender);
