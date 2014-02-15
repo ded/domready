e462a61ed080cd8f225c9513fa96266a6c51a227
Dustin Diaz <polvero@gmail.com>
2014-02-14 22:06:58 -0800

    new domready deprecating old IE6,7,8 support

--- src/ready.js
+++ src/ready.js
@@ -9,47 +9,23 @@
 
   var fns = [], fn, f = false
     , doc = document
-    , testEl = doc.documentElement
-    , hack = testEl.doScroll
     , domContentLoaded = 'DOMContentLoaded'
     , addEventListener = 'addEventListener'
     , onreadystatechange = 'onreadystatechange'
     , readyState = 'readyState'
-    , loadedRgx = hack ? /^loaded|^c/ : /^loaded|c/
-    , loaded = loadedRgx.test(doc[readyState])
+    , loaded = /^loaded|c/.test(doc[readyState])
 
   function flush(f) {
     loaded = 1
     while (f = fns.shift()) f()
   }
 
-  doc[addEventListener] && doc[addEventListener](domContentLoaded, fn = function () {
+  doc[addEventListener](domContentLoaded, fn = function () {
     doc.removeEventListener(domContentLoaded, fn, f)
     flush()
   }, f)
 
-
-  hack && doc.attachEvent(onreadystatechange, fn = function () {
-    if (/^c/.test(doc[readyState])) {
-      doc.detachEvent(onreadystatechange, fn)
-      flush()
-    }
-  })
-
-  return (ready = hack ?
-    function (fn) {
-      self != top ?
-        loaded ? fn() : fns.push(fn) :
-        function () {
-          try {
-            testEl.doScroll('left')
-          } catch (e) {
-            return setTimeout(function() { ready(fn) }, 50)
-          }
-          fn()
-        }()
-    } :
-    function (fn) {
-      loaded ? fn() : fns.push(fn)
-    })
+  return function (fn) {
+    loaded ? fn() : fns.push(fn)
+  }
 })
