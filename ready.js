92d60be2763834d8f2fda0e75be2f20c105c196c
Matthew Cudmore <cudmore.mb@gmail.com>
2014-02-20 23:14:21 -0400

    Rename variables and add whitespace for clarity

--- ready.js
+++ ready.js
@@ -2,28 +2,30 @@
   * domready (c) Dustin Diaz 2014 - License MIT
   */
 !function (name, definition) {
+
   if (typeof module != 'undefined') module.exports = definition()
   else if (typeof define == 'function' && typeof define.amd == 'object') define(definition)
   else this[name] = definition()
+
 }('domready', function () {
 
-  var fns = [], fn, f = false
+  var fns = [], listener
     , doc = document
     , domContentLoaded = 'DOMContentLoaded'
-    , onreadystatechange = 'onreadystatechange'
     , loaded = /^loaded|^c/.test(doc.readyState)
 
-  function flush(f) {
+  function flush (fn) {
     loaded = 1
-    while (f = fns.shift()) f()
+    while (fn = fns.shift()) fn()
   }
 
-  doc.addEventListener(domContentLoaded, fn = function () {
-    doc.removeEventListener(domContentLoaded, fn, f)
+  doc.addEventListener(domContentLoaded, listener = function () {
+    doc.removeEventListener(domContentLoaded, listener)
     flush()
-  }, f)
+  })
 
   return function (fn) {
     loaded ? fn() : fns.push(fn)
   }
+
 });
